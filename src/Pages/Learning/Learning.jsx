a2452b29d22b992579ba6ad51361be8ed1830710
mohimanul15 <mohimanul15@gmail.com>
2025-05-02 04:42:45 +0600

    Learn page complete

--- src/Pages/Learning/Learning.jsx
+++ src/Pages/Learning/Learning.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { Link, useLoaderData } from 'react-router';
 import Lessons from '../../Layout/Components/Lessons/Lessons';
+import rusflag from '../../assets/image/flagrus.jpg';
+import YouTube from 'react-youtube';
 
 const Learning = () => {
+
+    const options = {
+        playerVars: {
+            autoplay: 1,
+            controls: 1,
+        },
+    };
 
     const lesson = useLoaderData();
 
@@ -27,10 +36,36 @@ const Learning = () => {
             </section>
 
             {/* Tutorial content */}
-            <section className='bg-neutral-300 place-content-center'>
-                    <div>
-
+            <section className='bg-linear-to-br from-blue-900 to-purple-950 place-content-center max-w-6xl mx-auto rounded-2xl my-10 py-15 px-3'>
+                <div className='flex gap-8 flex-col lg:flex-row'>
+                    <div className='mx-auto rounded-3xl overflow-clip w-full h-auto relative'>
+                        <YouTube
+                            videoId='tYDMSM5dosE'
+                            options={options}
+                            className='opacity-90'
+                            style={{aspectRatio:16/9 + 'auto'}}
+                        >
+                        </YouTube>
                     </div>
+
+                    <div className='bg-white rounded-2xl p-5'>
+                        <h3 className='font-extrabold text-neutral-900 text-lg md:text-2xl lg:text-4xl max-md:text-center'>
+                            Start by learning the alphabet
+                        </h3>
+
+                        <p className='text-neutral-700 text-xs md:text-lg max-md:text-center max-md:max-w-lg max-md:mx-auto my-3'>
+                            Mastering the Cyrillic alphabet is your first step toward unlocking the Russian language. Don’t worry—it’s easier than you think! With our interactive approach, you’ll be reading signs, menus, and messages in no time.
+                        </p>
+
+                        <div className="justify-center md:justify-start card-actions mt-4">
+                            <Link to={'/tutorial'}>
+                                <button className="btn btn-primary">
+                                    View More
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     );
